dc3a07b604b97ddb2bf775799fb40f4af8171053
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-01-08 15:54:07 -0800

    added libraries
    rev

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,53 +4,74 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
   public static class SwerveConstants {
     /**
      * The left-to-right distance between the drivetrain wheels
      *
      * Should be measured from center to center.
      */
-    //17.1 inches
-    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.59055; 
+    // 17.1 inches
+    public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.447675;
     /**
      * The front-to-back distance between the drivetrain wheels.
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.59055;   
+    public static final double DRIVETRAIN_WHEELBASE_METERS = 0.447675;
 
-    //public static final int DRIVETRAIN_PIGEON_ID = 0; //   Set Pigeon ID
+    // public static final int DRIVETRAIN_PIGEON_ID = 0; // Set Pigeon ID
+    public static SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+        // Front left
+        new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+        // Front right
+        new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0),
+        // Back left
+        new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+        // Back right
+        new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0));
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 5;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 6;
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 11;
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(307.4); // Measure and set front left
+                                                                                        // steer offset
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; 
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9; 
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(330.0); //   Measure and set front left steer offset
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 7;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 8;
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12;
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(111.5); // Measure and set front right
+    // steer offset
 
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3; 
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4; 
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10; 
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(8.2); //   Measure and set front right steer offset
+    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3;
+    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 4;
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10; // Set back left steer encoder ID
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(252.3); // Measure and set back left
+                                                                                       // steer
+                                                                                       // offset
 
-    public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
-    public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11; //   Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(24.6); //   Measure and set back left steer offset
-
-    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; 
-    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12; //   Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(309.0); //   Measure and set back right steer offset
+    public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 1;
+    public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 2;
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 9; // Set back right steer encoder ID
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(150); // Measure and set back right
+                                                                                      // steer offset
   }
 }
