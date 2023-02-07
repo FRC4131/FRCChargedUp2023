19f6968ccd70278dd6de867ef438f846d14896f4
AnimeKitty meow meow <ianftw3@gmail.com>
2023-02-06 17:14:57 -0800

    paths
    made paths

--- src/main/java/frc/robot/commands/ArmJoystickCommand.java
+++ src/main/java/frc/robot/commands/ArmJoystickCommand.java
@@ -29,7 +29,9 @@ public class ArmJoystickCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_ArmSubsystem.resetPosition();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
