48f7008c4ea490ea117fb6fbd5d9957d28d978d7
sfotware <70352577+JunMalit@users.noreply.github.com>
2023-02-13 17:39:21 -0800

    Wrist stuff

--- src/main/java/frc/robot/commands/WristCommand.java
+++ src/main/java/frc/robot/commands/WristCommand.java
@@ -4,24 +4,12 @@
 
 package frc.robot.commands;
 
-import java.util.function.DoubleSupplier;
-
-import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ClawSubsystem;
 
-public class ClawPowerCommand extends CommandBase {
-
-  ClawSubsystem m_claw;
-  int direction;
-
-  /** Creates a new ClawPowerCommand. */
-  public ClawPowerCommand(ClawSubsystem clawsub, int dir) {
-    m_claw = clawsub;
-    direction = dir;
-
+public class WristCommand extends CommandBase {
+  /** Creates a new WristCommand. */
+  public WristCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(clawsub);
   }
 
   // Called when the command is initially scheduled.
@@ -30,15 +18,11 @@ public class ClawPowerCommand extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_claw.intakeSpeed(.6 * direction);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_claw.intakeSpeed(0);
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
