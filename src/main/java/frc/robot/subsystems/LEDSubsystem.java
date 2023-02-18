034ba239a32c65432f0b938817f86caf29ca0cd5
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-02-17 16:50:35 -0800

    ADDED BASIC LEDSUBSYSTEM STUFF
    ADDED LEDSUBSYSTEM AND DEFAULTLEDCOMMAND

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -2,22 +2,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 // Code based on the old code from frc2022
 
 public class LEDSubsystem extends SubsystemBase {
-    private AddressableLED m_LED = new AddressableLED(0);
-    private AddressableLEDBuffer m_LEDBuffer = new AddressableLEDBuffer(62);
-    int h = 130;
-    int s = 255;
-    int v = 255;
-
+    private AddressableLED m_LED = new AddressableLED(Constants.LEDConstants.PORT_1);
+    private AddressableLEDBuffer m_LEDBuffer = new AddressableLEDBuffer(Constants.LEDConstants.NUMBER_LEDS);
 
     public LEDSubsystem() {
-        m_LEDBuffer.setHSV(0, 130, 255, 255);
         m_LED.setLength(m_LEDBuffer.getLength());
         m_LED.setData(m_LEDBuffer);
         m_LED.start();
@@ -29,37 +23,11 @@ public class LEDSubsystem extends SubsystemBase {
     }
 
     // If ledIndex is set to -1, then all LEDs will be set to the provided h, s, and v values
-
-    public int getH()
-    {
-        return h;
-    }
-
-    public int getS()
-    {
-        return s;
-    }
-
-    public int getV()
-    {
-        return v;
-    }
-    
-    /**
-     * Sets HSV values for the specified LED index.
-     * <p><i>Use -1 as the selected index to change all lights
-     * @param ledIndex selected LED
-     * @param initH hue value [0,180)
-     * @param initS saturation value [0,255]
-     * @param initV value value [0, 255]
-     */
     public void setHSV(int ledIndex, int h, int s, int v) {
         if (ledIndex == -1) {
-            for (int i = 0; i < 62; i++) {
-                SmartDashboard.putNumber("LED index", i);
+            for (int i = 0; i < m_LEDBuffer.getLength(); ++i) {
                 m_LEDBuffer.setHSV(i, h, s, v);
             }
-            return;
         }
         else {
             m_LEDBuffer.setHSV(ledIndex, h, s, v);
