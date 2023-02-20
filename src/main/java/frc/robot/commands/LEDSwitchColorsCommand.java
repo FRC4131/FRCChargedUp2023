d48e9104e03197161148ad817fcfb27a503fece5
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-02-20 15:44:33 -0800

    LEDSwitchCommand

--- src/main/java/frc/robot/commands/LEDSwitchColorsCommand.java
+++ src/main/java/frc/robot/commands/LEDSwitchColorsCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LEDSubsystem;
 
 public class LEDSwitchColorsCommand extends CommandBase {
   private final LEDSubsystem m_LEDSubsystem;
@@ -17,11 +18,22 @@ public class LEDSwitchColorsCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_LEDSubsystem.setHSV(-1, 33, 58, 99);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (m_LEDSubsystem.getH() == 60 && m_LEDSubsystem.getS() == 100 && m_LEDSubsystem.getV() == 100)
+    {
+      m_LEDSubsystem.setHSV(-1, 291, 90, 70);
+    }
+    else if (m_LEDSubsystem.getH() == 291 && m_LEDSubsystem.getS() == 90 && m_LEDSubsystem.getV() == 70)
+    {
+      m_LEDSubsystem.setHSV(-1, 60, 100, 100);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
