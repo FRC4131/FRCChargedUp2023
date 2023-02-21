a6e88bd1f0779580b2d824eece8429e7f9e468fe
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-02-21 15:16:34 -0800

    undo arm change

--- src/main/java/frc/robot/commands/ArmJoystickCommand.java
+++ src/main/java/frc/robot/commands/ArmJoystickCommand.java
@@ -29,7 +29,6 @@ public class ArmJoystickCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_ArmSubsystem.resetPosition();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
