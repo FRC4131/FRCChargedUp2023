3bde4c4db7bd7bfc1e6377d96647d69b4c83e628
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-02-21 15:18:08 -0800

    Paths (#7)
    * paths
    ---------
    
    Co-authored-by: Ian <ianftw3@gmail.com>

--- src/main/java/frc/robot/commands/ArmJoystickCommand.java
+++ src/main/java/frc/robot/commands/ArmJoystickCommand.java
@@ -28,7 +28,8 @@ public class ArmJoystickCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
