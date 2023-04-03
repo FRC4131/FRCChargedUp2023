aae1720a0d2fad58de9dc5cc7c21197e12e027de
JunMalit <70352577+JunMalit@users.noreply.github.com>
2023-04-03 15:32:24 -0700

    Update Constants.java
    Experimental 10% max speed increase

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -245,7 +245,7 @@ public final class Constants {
 
         /* Swerve Profiling Values */
         /** Meters per Second */
-        public static final double maxSpeed = 4.4; // TODO: This must be tuned to specific robot
+        public static final double maxSpeed = 4.8; // TODO: This must be tuned to specific robot
         /** Radians per Second */
         public static final double maxAngularVelocity = 10.0; // TODO: This must be tuned to specific robot
 
